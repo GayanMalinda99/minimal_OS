52f94239720ead432fe3b9f2b1fe82dcbddcfeb4
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2021-08-13 13:40:47 +0530

    Add files via upload (#4)

--- kmain.c
+++ kmain.c
@@ -1,10 +1,14 @@
-#include "frameBuffer.h"
-#include "serialPort.h"
+#include "frame_buffer.h"
+#include "serial_port.h"
+#include "memory_segments.h"
 
 
 int main()
 {
     char consoleOut[] = "Hello World"; 
+    
     serial_write(0x3F8, consoleOut, sizeof(consoleOut));
     fb_write(consoleOut, sizeof(consoleOut));
+    segments_install_gdt();
+    
 }
