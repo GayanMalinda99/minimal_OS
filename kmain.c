b3dff194faea47de3fe0fec030d9abf9c5816dcb
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2021-08-27 16:31:06 +0530

    Add files via upload (#7)

--- kmain.c
+++ kmain.c
@@ -4,23 +4,21 @@
 #include "memory_segments.h"
 #include "keyboard.h"
 #include "interrupts.h"
+#include "multiboot.h"
+#include "start_program.h"
+
+/*function to intialize interrupts and segments*/
+void init_segments_interrupts(){
+	segments_install_gdt();
+	interrupts_install_idt();
+
+}
+
+/*kernal main funcion*/
+void kmain(unsigned int ebx){
+
+	init_segments_interrupts();   //initialize interrunpts and segments
+	run_custom_program(ebx);      //run the user program
 
 
-    
-void kmain(){
-
-    //char consoleOut[] = "Hello World";
-    unsigned char scancode,ascii;
-    char asciicode[1];
-   
-    segments_install_gdt();
-    //serial_write(0x3F8, consoleOut, sizeof(consoleOut));
-    //fb_write(consoleOut, sizeof(consoleOut));
-    interrupts_install_idt();
-    scancode = keyboard_read_scan_code();
-    ascii = keyboard_scan_code_to_ascii(scancode);
-    asciicode[0] = ascii;
-    serial_write(asciicode, sizeof(asciicode));
-    //fb_write(asciicode, sizeof(asciicode));
-    
 }
